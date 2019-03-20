e918d8f9332298651bd64da084da9ae4d8d814e9
124176018 <124176018@qq.com>
2019-03-20 12:33:25 +0800

    备份

---
+++ src/main/java/com/aishang/dao/UserMapper.java
@@ -0,0 +1,8 @@
+package com.aishang.dao;
+
+public interface UserMapper {
+    /*往user表里添加用户信息的方法*/
+     void addUser(User user);
+    /*根据用户名查询user表里是否有这个用户为AXAJ做准备*/
+    User findUserByName(String username);
+}
---
+++ src/main/java/com/aishang/service/UserService.java
@@ -0,0 +1,5 @@
+package com.aishang.service;
+
+public class UserService implements IUserService{
+
+}
